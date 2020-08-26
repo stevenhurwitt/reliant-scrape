c58d141b8d06b9af691a466b163a0bfc3e05150a
Steven Hurwitt <steven.hurwitt@exstratus.com>
2020-08-26 08:02:37 -0500

    init-repo

---
+++ reliant_scrape.py
@@ -0,0 +1,79 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+import selenium.webdriver.support.ui as ui
+from selenium.webdriver.common.keys import Keys
+import selenium.webdriver as webdriver
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import selenium
+import html5lib
+import json
+import os
+
+def logon(headless, download_path, url, creds):
+    """logon to reliant site.
+    
+    keyword arguments:
+    headless (bool) - whether webscrape should be headless or not
+    download_path (str) - filepath to save downloads
+    url (str) - url to log on to
+    creds (str) - filepath of user/password credentials
+    
+    returns:
+    browser (selenium.WebDriver) - selenium webdriver object
+    """
+    
+    opts = Options()
+    opts.add_argument('--no-sandbox')
+    opts.add_argument('--ignore-certificate-errors')
+    opts.add_argument('--start-maximized')
+    opts.add_argument('--disable-dev-shm-usage')
+    #opts.binary_location = '/usr/bin/google-chrome-stable'
+    
+    with open(creds, 'r') as f:
+        creds = json.load(f)
+
+    if headless:
+        opts.add_argument('--headless')
+        assert opts.headless
+        
+        def enable_download_headless(browser, download_dir):
+            browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+            params = {'cmd':'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+            browser.execute("send_command", params)
+     
+        prefs = {
+            'download.default_directory': download_path,
+            'download.prompt_for_download': False,
+            'download.directory_upgrade': True,
+            'safebrowsing.enabled': False,
+            'safebrowsing.disable_download_protection': True}
+
+    else:
+        prefs = {
+            'download.prompt_for_download': False,
+            'safebrowsing.enabled': False,
+            'safebrowsing.disable_download_protection': True}
+
+    opts.add_experimental_option("prefs", prefs)
+
+    browser = Chrome(executable_path = '/usr/local/bin/chromedriver', options = opts)
+    
+    if download_path and headless:
+        enable_download_headless(browser, download_path)
+        
+    browser.get('https://www.reliant.com/public/altLogon.htm')
+    
+    user = browser.find_element_by_xpath("//input[@id='altLoginUsername']")
+    checkbox = browser.find_element_by_xpath("//input[@id='altRememberMe']")
+    password = browser.find_element_by_xpath("//input[@id='altLoginPassword']")
+    
+    user.send_keys(creds['user'])
+    password.send_keys(creds['password'])
+    
+    logon = browser.find_element_by_css_selector("button[class*=myaccount-btn]")
+    logon.click()
+    
+    return(browser)
