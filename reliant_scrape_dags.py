7250c5e170832452b2f45a19a5097eb582a05026
Steven Hurwitt <stevenhurwitt@gmail.com>
2020-10-20 20:24:17 -0500

    start airflow

---
+++ reliant_scrape_dags.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from sqlalchemy import create_engine, types
+import selenium.webdriver.support.ui as ui
+import selenium.webdriver as webdriver
+from selenium.webdriver import Chrome
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import mysql.connector
+import pandas as pd
+import numpy as np
+import selenium
+import html5lib
+import yaml
+import json
+import time
+import sys
+import os
+
